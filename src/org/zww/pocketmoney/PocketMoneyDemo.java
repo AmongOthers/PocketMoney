04b3d04b048400d487bcadd4cf9b86143711f230
amongothers <zhengwenwei.123@gmail.com>
2013-01-18 12:09:32 +0800

    init

---
+++ src/org/zww/pocketmoney/PocketMoneyDemo.java
@@ -0,0 +1,13 @@
+package org.zww.pocketmoney;
+
+public class PocketMoneyDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
